1e01d238f84108e58206f6ba0b941033cd3b7359
PHILLIPBANK\Ssong <ssong@phillipbank.com.kh>
2018-01-19 09:10:34 +0700

    change menu default

--- src/main/webapp/js/application.js
+++ src/main/webapp/js/application.js
@@ -1,4 +1,4 @@
 $(document).ready(function(){
-    $('body').addClass('skin-blue sidebar-mini wysihtml5-supported sidebar-collapse');
+    $('body').addClass('skin-blue sidebar-mini wysihtml5-supported');
 });
 $.widget.bridge('uibutton', $.ui.button);
