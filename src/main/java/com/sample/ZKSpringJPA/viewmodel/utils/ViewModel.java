b5797a772c9ec6b0285cdb282fd06f105731e6bb
PHILLIPBANK\Ssong <ssong@phillipbank.com.kh>
2018-01-25 17:20:55 +0700

    fix #30 404 issue

--- src/main/java/com/sample/ZKSpringJPA/viewmodel/utils/ViewModel.java
+++ src/main/java/com/sample/ZKSpringJPA/viewmodel/utils/ViewModel.java
@@ -1,8 +1,13 @@
 package com.sample.ZKSpringJPA.viewmodel.utils;
 
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
 import org.zkoss.bind.BindUtils;
 
 public abstract class ViewModel {
+	
+	public static final Logger logger = LogManager.getLogger(ViewModel.class);
+	
     public void postNotifyChange(final Object bean, final String property) {
         BindUtils.postNotifyChange(null, null, bean, property);
     }
